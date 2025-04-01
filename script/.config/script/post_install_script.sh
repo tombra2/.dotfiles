96647473b03877379445e3f215e95fe368283852
tom_brandi <brandner.thomas@me.com>
2025-04-01 16:15:41 +0200

    Update script

---
+++ script/.config/script/post_install_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# zapzap discord thunderbird todoist teams spotify 1password und 1password cli
+# google-chrome kitty yay i3 tmux neovim stow jetbrainstoolbox zip unzip tree lza
+# zsh oh-my-psh oh-my-zsh, nerd font sdkman clang pyhton pip pyenv php composer
+# docker-compose docker-buildx shutter
+#
+#
